a4aaff09875b2989ed60d9daa0b92d46d0e631e1
happy-demon <fei@sgts.us>
2015-04-05 15:34:47 -0700

    Added Table Storage module
    Added Table Storage module

--- Outlets.py
+++ Outlets.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import pyodbc
+#import pyodbc
 from translate import Translator
 from datetime import datetime
 import re
+from azure.storage import TableService, Entity
 
 source_code = requests.get(
     "http://www.premiumoutlets.com/outlets/sales.asp?id=71")
@@ -19,17 +20,21 @@ coupons = web_data_str.split(
 
 translator= Translator(to_lang="zh")
 
-cnxn = pyodbc.connect(
-    'DRIVER={SQL Server};SERVER=xwmilyml1x.database.windows.net;\
-    DATABASE=Coupon;UID=SGTSUser;PWD=Abc1234$')
-cursor = cnxn.cursor()
+#Comment out azure SQL part as we decide to use Table Storage
+#cnxn = pyodbc.connect(
+#    'DRIVER={SQL Server};SERVER=xwmilyml1x.database.windows.net;\
+#    DATABASE=Coupon;UID=SGTSUser;PWD=Abc1234$')
+#cursor = cnxn.cursor()
+
+table_service = TableService(account_name='migodata',\
+                             account_key='lSx7cYTgHBYZ0rG0A5fBf/IIhpjlXTp9ULT/Fvst0oVsY82biTuZAlqZP2sakS/730PnaHAg4u56547Y6g9mTQ==')
 
 for coupon in coupons:
     coupon = coupon.replace("<h4 class=\"cap mb-10\">","")\
              .replace("</h4>","").replace("<br>","").replace("</br>","")
     coupon_split = re.split('\r', coupon)
     coupon_split = filter(None, coupon_split)
-    store_name = coupon_split[0]
+    store_name = coupon_split[0].replace("&amp;","&").replace("\n","")
     date_string = coupon_split[1].replace(",","").replace("\n","")
     coupon_split.pop(0)
     coupon_split.pop(0)
@@ -53,11 +58,24 @@ for coupon in coupons:
                           date_string.split(" ")[3] + ' ' + date_string.split(" ")[-1]
         start_date = datetime.strptime(start_date_string, '%b %d %Y')
         end_date = datetime.strptime(end_date_string, '%b %d %Y')
-        cursor.execute('INSERT INTO [dbo].[Stores] \
-                        ([store_name],[start_date],[end_date],[description],[description_cn]) \
-                        values (?,?,?,?,?)', \
-                       store_name,start_date,end_date,description,description_cn)
-        cnxn.commit()
+        
+        #Comment out azure SQL part as we decide to use Table Storage
+        #cursor.execute('INSERT INTO [dbo].[Stores] \
+        #                ([store_name],[start_date],[end_date],[description],[description_cn]) \
+        #                values (?,?,?,?,?)', \
+        #               store_name,start_date,end_date,description,description_cn)
+        #cnxn.commit()
+
+        #Insert data to Table Storage
+        task = Entity()
+        task.PartitionKey = 'Outlets'
+        task.RowKey = store_name
+        task.StartDate = start_date
+        task.EndDate = end_date
+        task.Description = description
+        task.Description_CN = description_cn
+        table_service.insert_entity('Outlets', task)
+        
         print(store_name)
         print(start_date)
         print(end_date)
@@ -71,11 +89,24 @@ for coupon in coupons:
                           date_string.split(" ")[4] + ' ' + date_string.split(" ")[-1]
         start_date = datetime.strptime(start_date_string, '%b %d %Y')
         end_date = datetime.strptime(end_date_string, '%b %d %Y')
-        cursor.execute('INSERT INTO [dbo].[Stores] \
-                        ([store_name],[start_date],[end_date],[description],[description_cn]) \
-                        values (?,?,?,?,?)', \
-                       store_name,start_date,end_date,description,description_cn)
-        cnxn.commit()
+
+        #Comment out azure SQL part as we decide to use Table Storage
+        #cursor.execute('INSERT INTO [dbo].[Stores] \
+        #                ([store_name],[start_date],[end_date],[description],[description_cn]) \
+        #                values (?,?,?,?,?)', \
+        #               store_name,start_date,end_date,description,description_cn)
+        #cnxn.commit()
+        
+        #Insert data to Table Storage
+        task = Entity()
+        task.PartitionKey = 'Outlets'
+        task.RowKey = store_name
+        task.StartDate = start_date
+        task.EndDate = end_date
+        task.Description = description
+        task.Description_CN = description_cn
+        table_service.insert_entity('Outlets', task)
+
         print(store_name)
         print(start_date)
         print(end_date)
@@ -89,11 +120,24 @@ for coupon in coupons:
                           date_string.split(" ")[5] + ' ' + date_string.split(" ")[-1]
         start_date = datetime.strptime(start_date_string, '%b %d %Y')
         end_date = datetime.strptime(end_date_string, '%b %d %Y')
-        cursor.execute('INSERT INTO [dbo].[Stores] \
-                        ([store_name],[start_date],[end_date],[description],[description_cn]) \
-                        values (?,?,?,?,?)', \
-                       store_name,start_date,end_date,description,description_cn)
-        cnxn.commit()
+
+        #Comment out azure SQL part as we decide to use Table Storage
+        #cursor.execute('INSERT INTO [dbo].[Stores] \
+        #                ([store_name],[start_date],[end_date],[description],[description_cn]) \
+        #                values (?,?,?,?,?)', \
+        #               store_name,start_date,end_date,description,description_cn)
+        #cnxn.commit()
+
+        #Insert data to Table Storage
+        task = Entity()
+        task.PartitionKey = 'Outlets'
+        task.RowKey = store_name
+        task.StartDate = start_date
+        task.EndDate = end_date
+        task.Description = description
+        task.Description_CN = description_cn
+        table_service.insert_entity('Outlets', task)
+        
         print(store_name)
         print(start_date)
         print(end_date)
